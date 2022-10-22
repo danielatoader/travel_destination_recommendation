1549162cd16c67758f9356143356db3bcda12d9c
Daniela Toader <daniela.toader07@gmail.com>
2022-10-22 22:01:30 +0200

    Use dataframes instead of mongodb

--- scrape.py
+++ scrape.py
@@ -3,6 +3,7 @@ import pandas as pd
 import time
 import urllib.request
 import numpy as np
+from selenium.webdriver.common.by import By
 
 
 def collect_urls(browser, home_path):
@@ -11,7 +12,7 @@ def collect_urls(browser, home_path):
     """
     urls = []
     links = [a.get_attribute('href')
-             for a in browser.find_elements_by_tag_name('a')]
+             for a in browser.find_elements("tag name", 'a')]
     for link in links:
         if home_path in link and link not in urls:
             urls.append(link)
@@ -28,9 +29,10 @@ def collect_all_data(browser, country_url,
     country = collect_country_name(browser)
     country_summary = collect_country_summary(browser)
     
-    # to mongodb collection
+    # alternative to mongodb collection
     country_dict = {'country': country, 'country_summary': country_summary}
-    country_collection.insert_one(country_dict)
+    # country_collection.insert_one(country_dict)
+    country_collection[len(country_collection)] = country_dict
     
     time.sleep(np.random.randint(10, 60))
     print(f'Inserted {country} into country collection')
@@ -41,18 +43,20 @@ def collect_all_data(browser, country_url,
         time.sleep(np.random.randint(10, 30))
     print(f'Completed scraping {country}')
 
+    # return country_collection, city_collection
+
 
 def collect_country_name(browser):
     """Collects the country name from ricksteves.com"""
     country_path = '//*[@id="body"]/div[2]/div[1]/div/h1'
-    country = browser.find_element_by_xpath(country_path).text
+    country = browser.find_element(By.XPATH, country_path).text
     return country
 
 
 def collect_country_summary(browser):
     """Collects the country summary from ricksteves.com"""
     summary_path = '//*[@id="body"]/div[2]/div[1]/div/div[3]/p'
-    country_summary = browser.find_element_by_xpath(summary_path).text
+    country_summary = browser.find_element(By.XPATH, summary_path).text
     return country_summary
 
 
@@ -69,8 +73,13 @@ def collect_city_data(browser, city_url, country, city_collection):
         city_dict = {'city': city, 'country': country,
                      'city_summary': summary,
                      'city_url': city_url}
-        city_collection.insert_one(city_dict)
+        # alternative to mongodb
+        # city_collection.insert_one(city_dict)
+        city_collection[len(city_collection)] = city_dict
+
         print(f'Inserted {city}, {country} into city collection')
+
+        # return city_collection
     except:
         None
 
@@ -78,7 +87,7 @@ def collect_city_data(browser, city_url, country, city_collection):
 def collect_city_name(browser):
     """Collects the city name from ricksteves.com"""
     city_path = '//*[@id="body"]/div[2]/div[1]/div/h1'
-    city = browser.find_element_by_xpath(city_path).text
+    city = browser.find_element(By.XPATH, city_path).text
     return city
 
 
@@ -87,7 +96,7 @@ def collect_city_summary(browser):
     Collects the city summary from ricksteves.com
     """
     summary_path = '//*[@id="body"]/div[2]/div[1]/div/div[3]/p'
-    summary = browser.find_element_by_xpath(summary_path).text
+    summary = browser.find_element(By.XPATH, summary_path).text
     return summary
 
 
@@ -97,7 +106,7 @@ def collect_city_photo(browser, country_url):
     """
     browser.get(country_url)
     country_path = '//*[@id="body"]/div[2]/div[1]/div/h1'
-    country = browser.find_element_by_xpath(country_path).text
+    country = browser.find_element(By.XPATH, country_path).text
 
     city_path = '/europe/' + country.lower() + '/'
     city_urls = collect_urls(browser, city_path)
@@ -106,9 +115,9 @@ def collect_city_photo(browser, country_url):
         time.sleep(np.random.randint(10, 30))
         try:
             city_path = '//*[@id="body"]/div[2]/div[1]/div/h1'
-            city = browser.find_element_by_xpath(city_path).text
+            city = browser.find_element(By.XPATH, city_path).text
             image_path = '//*[@id="slideshow1_s0"]/figure/img'
-            image = browser.find_element_by_xpath(image_path)
+            image = browser.find_element(By.XPATH, image_path)
             img_url = image.get_attribute('src')
             filename = 'images/' + city.lower() + '.jpg'
             urllib.request.urlretrieve(str(img_url), filename=str(filename))
@@ -130,12 +139,15 @@ def get_wiki_description(browser, city, wiki_collection):
     for i in range(1, 100):
         paragraphs = '//*[@id="mw-content-text"]/div/p[' + str(i) + ']'
         try:
-            summary += browser.find_element_by_xpath(paragraphs).text
+            summary += browser.find_element(By.XPATH, paragraphs).text
             summary += '\n'
         except:
             None
     wiki_dict = {'city': city, 'text': summary}
-    wiki_collection.insert_one(wiki_dict)
+
+    # alternative to mongodb
+    # wiki_collection.insert_one(wiki_dict)
+    wiki_collection[len(wiki_collection)] = wiki_dict
 
 
 def replace_df_text(browser, city, df):
@@ -149,7 +161,7 @@ def replace_df_text(browser, city, df):
     for i in range(1, 100):
         paragraphs = '//*[@id="mw-content-text"]/div/p[' + str(i) + ']'
         try:
-            summary += browser.find_element_by_xpath(paragraphs).text
+            summary += browser.find_element(By.XPATH, paragraphs).text
             summary += '\n'
         except:
             None
