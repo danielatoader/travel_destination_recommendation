f9a742b07fbb814449f262bf39a5c937d982574f
Daniela Toader <daniela.toader07@gmail.com>
2022-10-23 02:48:52 +0200

    Work with dfs in recommend file

--- travel_destination_recommendation/recommend.py
+++ travel_destination_recommendation/recommend.py
@@ -2,16 +2,16 @@ from sklearn.neighbors import NearestNeighbors
 import pickle
 import pandas as pd
 import numpy as np
-import pymongo
+# import pymongo
 
 # Connect to MongoDB Atlas Database
-with open('.secrets/password.txt', 'r') as f:
-    conn_string = f.read().strip()
+# with open('.secrets/password.txt', 'r') as f:
+#     conn_string = f.read().strip()
 
-mc = pymongo.MongoClient(conn_string)
-city_db = mc['city_database']
-user_coll = city_db['user_collection']
-user_satisfaction = city_db['user_satisfaction']
+# mc = pymongo.MongoClient(conn_string)
+# city_db = mc['city_database']
+# user_coll = city_db['user_collection']
+# user_satisfaction = city_db['user_satisfaction']
 
 
 def get_initial_user_score():
@@ -118,7 +118,7 @@ def get_updated_n_recommendation(user_score, cities, random_recs,
 
 
 def get_user_city_ratings(nn_model, cities, user_score,
-                          visited):
+                          visited, user_coll):
     """
     Asks user to rate 10 random cities.
     User Input Meanings:
@@ -144,7 +144,8 @@ def get_user_city_ratings(nn_model, cities, user_score,
                                            random_recs, nn_model,
                                            city_ratings, visited)
 
-    user_coll.insert_one(user_dict)
+    user_coll[len(user_coll)] = user_dict
+    # user_coll.insert_one(user_dict)
     return closest
 
 
@@ -162,8 +163,15 @@ def make_recommendations(cities_df, nn_model, visited_cities=None):
             \t-1 if you have visited and disliked
             \t0 if you have never been''')
 
+    user_dict = dict()
     closest = get_user_city_ratings(nn_model, cities_df,
-                                    user_score, visited_cities)
+                                    user_score, visited_cities, user_dict)
+    user_df = pd.DataFrame(user_dict)
+    # swap the columns with indexes
+    user_df = user_df.transpose()
+
+    pickle.dump(user_df, open('data/user_df.pkl', 'wb'))
+
     return closest[0:5]
 
 
@@ -176,4 +184,4 @@ def rate_recs(recommendations):
     for rec in recommendations:
         interest.append(int(input(str(rec[0]) + ': ')))
     satisfaction = {'satisfaction_score': sum(interest) / 5}
-    user_satisfaction.insert_one(satisfaction)
+    # user_satisfaction.insert_one(satisfaction)
